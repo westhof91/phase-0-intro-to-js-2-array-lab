5169be40e21eb6224c783d6ccc9382ea182c96b1
westhof91 <88407013+westhof91@users.noreply.github.com>
2021-09-01 08:49:38 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+   
+
+}
+function  prependCat(name) {
+
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+
+}    
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+function appendCat(name) {
+    return ['Milo', 'Otis', 'Garfield', 'Broom']
+}
+function prependCat(name) {
+    return ['Arnold', 'Milo', 'Otis', 'Garfield',]
+    cats.Spreadoperator(name)
+}
+function  removeFirstCat(name) {
+   return ['Otis', 'Garfield']
+}
+function removeLastCat(name) {
+    return['Milo', 'Otis']
+}
